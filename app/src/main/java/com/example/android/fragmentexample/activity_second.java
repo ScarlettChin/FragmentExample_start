901d2fb95ae0642bc78817db1c5588d88d207aeb
ScarlettChin <chinhe-wm19@student.tarc.edu.my>
2020-08-07 17:34:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/fragmentexample/activity_second.java
@@ -0,0 +1,105 @@
+package com.example.android.fragmentexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.android.fragmentexample1.MainActivity;
+import com.example.android.fragmentexample1.R;
+import com.example.android.fragmentexample1.SimpleFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the  factory method to
+ * create an instance of this fragment.
+ */
+class SecondActivity extends AppCompatActivity {
+    private Button mButton;
+    private boolean isFragmentDisplayed = false;
+    static final String STATE_FRAGMENT = "state_of_fragment";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_activity_second);
+        mButton = findViewById(R.id.open_button);
+        Button mPrevious = findViewById(R.id.previous);
+
+        mPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isFragmentDisplayed) {
+                    displayFragment();
+                } else {
+                    closeFragment();
+                }
+            }
+        });
+        if (savedInstanceState != null) {
+            isFragmentDisplayed =
+                    savedInstanceState.getBoolean(STATE_FRAGMENT);
+            if (isFragmentDisplayed) {
+                // If the fragment is displayed, change button to "close".
+                mButton.setText(R.string.close);
+            }
+        }
+    }
+
+    public void displayFragment() {
+        SimpleFragment simpleFragment = SimpleFragment.newInstance();
+        // Get the FragmentManager and start a transaction.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+        // Add the SimpleFragment.
+        fragmentTransaction.add(R.id.fragment_container,
+                simpleFragment).addToBackStack(null).commit();
+        // Update the Button text.
+        mButton.setText(R.string.close);
+        // Set boolean flag to indicate fragment is open.
+        isFragmentDisplayed = true;
+        // Set boolean flag to indicate fragment is open.
+
+    }
+
+    public void closeFragment() {
+        // Get the FragmentManager.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        // Check to see if the fragment is already showing.
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager
+                .findFragmentById(R.id.fragment_container);
+        if (simpleFragment != null) {
+            // Create and commit the transaction to remove the fragment.
+            FragmentTransaction fragmentTransaction =
+                    fragmentManager.beginTransaction();
+            int commit = fragmentTransaction.remove(simpleFragment).commit();
+        }
+        // Update the Button text.
+        mButton.setText(R.string.open);
+        // Set boolean flag to indicate fragment is closed.
+        isFragmentDisplayed = false;
+    }
+
+
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the state of the fragment (true=open, false=closed).
+        savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/com/example/android/fragmentexample1/fragment_activity_second.java
@@ -0,0 +1,6 @@
+package com.example.android.fragmentexample1;
+
+import android.app.Activity;
+
+public class fragment_activity_second extends Activity {
+}
